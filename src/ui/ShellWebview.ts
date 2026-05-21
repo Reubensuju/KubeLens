f01880d8322813de20f35142817e6a89229e2304
Reubensuju <reubensuju@gmail.com>
2026-05-20 22:35:23 -0700

    fix: restore unscoped xterm package CDN link

--- src/ui/ShellWebview.ts
+++ src/ui/ShellWebview.ts
@@ -272,8 +272,8 @@ export class ShellWebview {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link href="https://unpkg.com/@vscode/codicons/dist/codicon.css" rel="stylesheet" />
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@xterm/xterm@5.3.0/css/xterm.css" crossorigin="anonymous" />
-                <script src="https://cdn.jsdelivr.net/npm/@xterm/xterm@5.3.0/lib/xterm.js" crossorigin="anonymous"></script>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/xterm@5.3.0/css/xterm.css" crossorigin="anonymous" />
+                <script src="https://cdn.jsdelivr.net/npm/xterm@5.3.0/lib/xterm.js" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/@xterm/addon-fit@0.10.0/lib/addon-fit.min.js" crossorigin="anonymous"></script>
                 <script src="https://cdn.jsdelivr.net/npm/@xterm/addon-search@0.16.0/lib/addon-search.min.js" crossorigin="anonymous"></script>
                 <style>
